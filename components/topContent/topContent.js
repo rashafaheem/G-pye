bd42591929581ff7d8f3d2db6868667332dd1109
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-09-17 03:47:28 -0700

    Top Content

---
+++ components/topContent/topContent.js
@@ -0,0 +1,29 @@
+import Grid from "@mui/material/Grid";
+
+function topContent() {
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={12}>
+          <img
+            className="captainToadImage"
+            src="/captaintoad.jpg"
+            alt="captainToadImage"
+          />
+          <Grid item xs={12} className="topContentGrid">
+            <h1 className="title">Coding Made Easy</h1>
+            <p className="subtitle">
+              The fun way to learn programming and develop problem-solving &
+              critical thinking skills! Ages 5-18
+            </p>
+            <Grid item xs={12} className="topGrid">
+              <button className="getStartedButton">GET STARTED FOR FREE</button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default topContent;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import AppBar from "../components/appBar/appBar.js";
+import TopContent from "../components/topContent/topContent.js";
 
 export default function Home() {
   return (
     <div>
       <AppBar />
+      <TopContent />
     </div>
   );
 }
